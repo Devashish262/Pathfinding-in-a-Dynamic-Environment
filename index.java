ca0121f83f1893bc77c074d9c0a19d5dd6e7e859
Devashish tyagi <144609436+Devashish262@users.noreply.github.com>
2024-12-27 12:54:38 +0530

    Update index.java

--- index.java
+++ index.java
@@ -1,1 +1,23 @@
+import java.util.*;
 
+class Node implements Comparable<Node> {
+    int x, y;
+    double gCost, hCost;
+    Node parent;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.gCost = Double.MAX_VALUE;
+        this.hCost = 0;
+        this.parent = null;
+    }
+
+    public double fCost() {
+        return gCost + hCost;
+    }
+
+    @Override
+    public int compareTo(Node other) {
+        return Double.compare(this.fCost(), other.fCost());
+    }
