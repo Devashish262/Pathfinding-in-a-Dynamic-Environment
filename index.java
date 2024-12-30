aa8be69abff59e0b1bf7d1f4d0543356e1c92ef7
Devashish tyagi <144609436+Devashish262@users.noreply.github.com>
2024-12-30 18:19:12 +0530

    Update index.java

--- index.java
+++ index.java
@@ -4,7 +4,7 @@ class Node implements Comparable<Node> {
     int x, y;
     double gCost, hCost;
     Node parent;
-
+    
     public Node(int x, int y) {
         this.x = x;
         this.y = y;
@@ -12,23 +12,24 @@ class Node implements Comparable<Node> {
         this.hCost = 0;
         this.parent = null;
     }
-
+    
     public double fCost() {
         return gCost + hCost;
     }
-
+    
     @Override
     public int compareTo(Node other) {
         return Double.compare(this.fCost(), other.fCost());
     }
-
-     @Override
+    
+    @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
         if (obj == null || getClass() != obj.getClass()) return false;
         Node node = (Node) obj;
         return x == node.x && y == node.y;
     }
+    
     @Override
     public int hashCode() {
         return Objects.hash(x, y);
@@ -46,3 +47,60 @@ public class DynamicPathfinding {
         this.cols = cols;
         this.obstacles = new boolean[rows][cols];
     }
+    
+    public void addObstacle(int x, int y) {
+        if (isValid(x, y)) {
+            obstacles[x][y] = true;
+        }
+    }
+    
+    public void removeObstacle(int x, int y) {
+        if (isValid(x, y)) {
+            obstacles[x][y] = false;
+        }
+    }
+    
+    public List<Node> findPath(int startX, int startY, int endX, int endY) {
+        PriorityQueue<Node> openSet = new PriorityQueue<>();
+        Set<Node> closedSet = new HashSet<>();
+        Node startNode = new Node(startX, startY);
+        Node endNode = new Node(endX, endY);
+        
+        startNode.gCost = 0;
+        startNode.hCost = heuristic(startX, startY, endX, endY);
+        openSet.add(startNode);
+        
+        while (!openSet.isEmpty()) {
+            Node current = openSet.poll();
+            
+            if (current.equals(endNode)) {
+                return reconstructPath(current);
+            }
+            
+            closedSet.add(current);
+            
+            for (int i = 0; i < 4; i++) {
+                int neighborX = current.x + dx[i];
+                int neighborY = current.y + dy[i];
+                
+                if (!isValid(neighborX, neighborY) || obstacles[neighborX][neighborY]) {
+                    continue;
+                }
+                
+                Node neighbor = new Node(neighborX, neighborY);
+                if (closedSet.contains(neighbor)) {
+                    continue;
+                }
+                
+                double tentativeGCost = current.gCost + 1;
+                if (tentativeGCost < neighbor.gCost) {
+                    neighbor.gCost = tentativeGCost;
+                    neighbor.hCost = heuristic(neighborX, neighborY, endX, endY);
+                    neighbor.parent = current;
+                    
+                    if (!openSet.contains(neighbor)) {
+                        openSet.add(neighbor);
+                    }
+                }
+            }
+        }
